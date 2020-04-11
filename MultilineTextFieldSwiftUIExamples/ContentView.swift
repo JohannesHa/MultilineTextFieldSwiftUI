9a0e4145e349639d6d705aebee9d68ca6fa2d9f5
Johannes <johanneshage97@gmail.com>
2020-04-11 23:54:41 +0200

    Initialise project setup

---
+++ MultilineTextFieldSwiftUIExamples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MultilineTextFieldSwiftUIExamples
+//
+//  Created by Johannes Hagemann on 11.04.20.
+//  Copyright © 2020 Johannes Hagemann. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
